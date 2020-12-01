09d5ee7af6cde0d80e68a4d66fbd2e13018ac5a1
Martin Danek <martindanek4@gmail.com>
2020-12-01 22:47:20 +0100

    operace split na stringu II

--- lekce_4.py
+++ lekce_4.py
@@ -1,3 +1,8 @@
-muj_string = 'Mam nove auto'
-rozdelen_do_listu = muj_string.split()
+muj_string = 'a,u,t,o'
+rozdelen_do_listu = muj_string.split(',')
+print('muj string: ', muj_string)
 print(rozdelen_do_listu)
+muj_string = muj_string.strip('a,') # v promenne odstrani z leva do prvniho znaku, ktery neni argumentem. U stringu odstrani vse
+print(muj_string)
+
+
