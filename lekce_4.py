97ce6700b18df63c39b2f5ba9c95768fb1bbd1ab
Martin Danek <martindanek4@gmail.com>
2020-12-02 22:32:46 +0100

    while smyce

--- lekce_4.py
+++ lekce_4.py
@@ -28,19 +28,27 @@
 zelenina = ['brokolice', 'petrzel', 'celer', 'redkev', 'rajce', 'petrzel',
                 'okurka', 'salat', 'cervena_repa', 'paprika', 'petrzel']
 
-# Zjisti pocet pro 'petrzel'
-pocet_Petr = zelenina.count('petrzel')
-print('Amount of parsley:', pocet_Petr)
-# Zjisti index pro 'rajce'
-rajce_Index = zelenina.index('rajce')
-print('Rajce je na pozici: ', rajce_Index)
+# # Zjisti pocet pro 'petrzel'
+# pocet_Petr = zelenina.count('petrzel')
+# print('Amount of parsley:', pocet_Petr)
+# # Zjisti index pro 'rajce'
+# rajce_Index = zelenina.index('rajce')
+# print('Rajce je na pozici: ', rajce_Index)
+#
+# # 'zelenina' bez 'okurka'
+# zelenina.remove('okurka')
+# print("zelenina bez okurky", zelenina)
+#
+# # ZDE NIC NEPIS. Jde o kontrolu tveho zapisu :)
+# if pocet_Petr == 3 and rajce_Index == 4 and 'okurka' not in zelenina:
+# 	print('Skvělá práce, v metodách listů jsi přeborník!')
+# else:
+# 	print('Ještě si to jednou projdi, někde se vloudila chybka :(\nPokud si nejsi jistý, mrkni ještě jednou na tabulku s metodami')
 
-# 'zelenina' bez 'okurka'
-zelenina.remove('okurka')
-print("zelenina bez okurky", zelenina)
-
-# ZDE NIC NEPIS. Jde o kontrolu tveho zapisu :)
-if pocet_Petr == 3 and rajce_Index == 4 and 'okurka' not in zelenina:
-	print('Skvělá práce, v metodách listů jsi přeborník!')
-else:
-	print('Ještě si to jednou projdi, někde se vloudila chybka :(\nPokud si nejsi jistý, mrkni ještě jednou na tabulku s metodami')
+#smycka WHILE
+muj_string = 'while smyčky můžou býT neKonečné'
+while muj_string:
+    if muj_string[0].isupper():
+        print('Našel jsem velké písmeno:',muj_string[0])
+    muj_string = muj_string[1:]
+print('muj_string po while:_' + muj_string + '_')
