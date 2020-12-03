ba92ca4ade1688def34a8888153608bba7589668
Martin Danek <martindanek4@gmail.com>
2020-12-03 13:44:59 +0100

    instructions

---
+++ lekce_04_22.py
@@ -0,0 +1,9 @@
+# Napiš program, který z listu slova vybere nejdelší slovo.
+# vytiskne do terminálu tuple s tímto nejdelším slovem a jeho délkou.
+slova = [
+    'Python', 'is', 'a', 'widely', 'used',
+    'high-level', 'programming', 'language',
+    'for', 'general-purpose', 'programming,',
+    'created', 'by', 'Guido', 'van', 'Rossum',
+    'and', 'first', 'released', 'in', '1991.'
+]
