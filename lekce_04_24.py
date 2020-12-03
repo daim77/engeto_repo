303cf44f0ef54a3dec7796b202ed317229cb2278
Martin Danek <martindanek4@gmail.com>
2020-12-03 16:26:38 +0100

    instructions

---
+++ lekce_04_24.py
@@ -0,0 +1,12 @@
+# kalkulacka
+# Tvým úkolem v této části bude napsat jednoduchý skript, který bude umět provádět se dvěma zadanými hodnotami
+# volitelné matematické operace (součet, odčítání, násobení, dělení) Řekněme, že si uživatel
+# vybere cislo01 = 3 a cislo02 = 4. Dále si vybere, že chce provést operaci soucet. Výsledkem by měl
+# vypsaný výpočet 3 + 4 = 7.
+# Nejprve si ukážeme, jak bychom měli u tvorby takového kódu přemýšlet.
+# Na začátek budeme chtít pozdravit uživatele a říct, aby vepsal dvě libovolná čísla.
+# Další částí bude nadepsaný výběr operací, které může s čísly provádět.
+# Nakonec budeme potřebovat spojit operaci, kterou uživatel vybral s výpočtem, který má provést a následně vypsat.
+# Pomocí cyklu bychom chtěli zopakovat, aby se uživateli nabízely operace tak dlouho,
+# dokud je nebude chtít příkazem ukončit.
+
