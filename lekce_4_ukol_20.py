ddafd1049a906181ec8f2ee41c69369059028857
Martin Danek <martindanek4@gmail.com>
2020-12-03 12:54:30 +0100

    str methods

--- lekce_4_ukol_20.py
+++ lekce_4_ukol_20.py
@@ -16,31 +16,34 @@ emaily.append(rozdeleny_string[2])
 print(emaily)
 #
 # # Ziskani domen
-# domena01 =
-# domena02 =
+domena01 = emaily[0].split('@')[-1]
+domena02 = emaily[1].split('@')[-1]
 #
 # # Rozdel domeny podle znaku '.' a uloz je do stejne prommene
-# domena01 =
-# domena02 =
-#
+domena01 = domena01.split('.')[0]
+domena02 = domena02.split('.')[0]
+
 # # Spoj promenne 'domena01' a 'domena02' s promennou 'bez_cisel'
-# bez_cisel = ''
+bez_cisel = ''
+
 # # Dopln podminkovy vyraz pro 'domena01'
-# if:
-#
-#
-# else:
-#
-#
+if domena01.isalpha():
+    bez_cisel = domena01.join(bez_cisel)
+    print('Domena', domena01, 'byla pridana')
+else:
+    print('Domena', domena01, 'nebyla pridana, protoze obsahuje cisla!')
+print(bez_cisel)
+
 # # Dopln podminkovy vyraz pro 'domena02'
-# if:
-#
-#
-# else:
-#
-#
-# # ZDE NIC NEDOPLNUJ, jde o nasi kontrolu :)
-# if (len(bez_cisel) + 1) % 4 == 0:
-#     print('Bezva, další zkouška úspěšně za tebou. Jen tak dál!')
-# else:
-#     print('Bohužel, někde máš chybku. Pokud si nevíš rady, mrkni se dolů na řešení.')
+if domena02.isalpha():
+    bez_cisel = domena02.join(bez_cisel)
+    print('Domena', domena02, 'byla pridana')
+else:
+    print('Domena', domena02, 'nebyla pridana, protoze obsahuje cisla!')
+print(bez_cisel)
+
+# ZDE NIC NEDOPLNUJ, jde o nasi kontrolu :)
+if (len(bez_cisel) + 1) % 4 == 0:
+    print('Bezva, další zkouška úspěšně za tebou. Jen tak dál!')
+else:
+    print('Bohužel, někde máš chybku. Pokud si nevíš rady, mrkni se dolů na řešení.')
