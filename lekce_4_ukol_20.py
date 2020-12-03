5dd120fa9bfc47b98f93b0b6ba1801ca659c3249
Martin Danek <martindanek4@gmail.com>
2020-12-03 09:46:06 +0100

    zadani lekce 4 ukolu 20

---
+++ lekce_4_ukol_20.py
@@ -0,0 +1,46 @@
+# Zadany string
+muj_string = 'Abc@abc.cz a Matous@1234.cz jsou naše emailové adresy'
+
+# # Rozdel string
+# rozdeleny_string =
+#
+# # Tisk promenne 'rozdeleny_string'
+# print(rozdeleny_string)
+#
+# # Ziskej emaily
+# emaily = list()
+#
+#
+#
+# # Tisk promenne 'emaily'
+# print(emaily)
+#
+# # Ziskani domen
+# domena01 =
+# domena02 =
+#
+# # Rozdel domeny podle znaku '.' a uloz je do stejne prommene
+# domena01 =
+# domena02 =
+#
+# # Spoj promenne 'domena01' a 'domena02' s promennou 'bez_cisel'
+# bez_cisel = ''
+# # Dopln podminkovy vyraz pro 'domena01'
+# if:
+#
+#
+# else:
+#
+#
+# # Dopln podminkovy vyraz pro 'domena02'
+# if:
+#
+#
+# else:
+#
+#
+# # ZDE NIC NEDOPLNUJ, jde o nasi kontrolu :)
+# if (len(bez_cisel) + 1) % 4 == 0:
+#     print('Bezva, další zkouška úspěšně za tebou. Jen tak dál!')
+# else:
+#     print('Bohužel, někde máš chybku. Pokud si nevíš rady, mrkni se dolů na řešení.')
