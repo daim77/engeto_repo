3769cb28b5554131905d2dd6e303d179502c2e26
Martin Danek <martindanek4@gmail.com>
2020-12-09 21:26:49 +0100

    intro

---
+++ lekce_06_26.py
@@ -0,0 +1,7 @@
+# Napiš program, který tiskne celá čísla od 1 do 100 (včetně).
+# Ale:
+# pro násobky 3 vytiskni Fizz (namísto čísla)
+# pro násobky 5 vytiskni Buzz (namísto čísla)
+# pro násobky 3 a 5 zároveň vytiskni FizzBuzz (namísto čísla)
+
+
