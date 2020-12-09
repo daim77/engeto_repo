1e8eb77403e263f203cc95b2c76891ca74414c66
Martin Danek <martindanek4@gmail.com>
2020-12-09 21:33:19 +0100

    fizzBuzz completed

--- lekce_06_26.py
+++ lekce_06_26.py
@@ -4,4 +4,12 @@
 # pro násobky 5 vytiskni Buzz (namísto čísla)
 # pro násobky 3 a 5 zároveň vytiskni FizzBuzz (namísto čísla)
 
-
+for i in range(1, 101, 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
