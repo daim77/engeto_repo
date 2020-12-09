7e89d1b9e8030766b3c7414595606f17d12d5fb1
Martin Danek <martindanek4@gmail.com>
2020-12-09 22:18:10 +0100

    intro

---
+++ lekce_06_27.py
@@ -0,0 +1,22 @@
+# CHESS
+# Napiš program, který vytiskne šachovnici daných rozměrů.
+# Program musí používat proměnné:
+# size - délka hrany šachovnice
+# symbols - znaky potřebné pro černá a bílá pole
+# desk - reprezentace šachovnice
+
+# Zadej rozměry šachovnice
+size = int(input('Zadej rozmer sachovnice: '))
+
+# Zadej symboly
+symbols = input('Zadej dva symboly sachovnice repre B&W: ')
+
+# Vytvoř šachovnici
+
+# Doplň smyčky for, které by měly postupně nahrát celou
+# šachovnici do proměnné 'desk'
+for a in range(size):
+    for b in range(size):
+    desk[a][b] =
+
+# Vytiskni šachovnici.
