7397e54306929dabb9a5a16e1477d2cc689209dc
Martin Danek <martindanek4@gmail.com>
2020-12-11 20:45:58 +0100

    solution

--- lekce_06_28.py
+++ lekce_06_28.py
@@ -13,23 +13,41 @@
 # Do proměnné daytime vyber odpovídající string z dvojčlenného listu ['AM', 'PM']
 # ======================================================================================
 
-# Získej vstup uživatele do proměnné `time`
-time = input('Insert time for conversion in format hh:mm :  ')
+# # Získej vstup uživatele do proměnné `time`
+# time = input('Insert time for conversion in format hh:mm :  ')
+#
+# # Rozděl do listu vstup od uživatele do proměnné `hours` a `mins`.
+# time = time.partition(':')
+# hours = time[0]
+# mins = time[2]
+#
+#
+# # Uprav proměnou `hours` tak, aby se do proměnné `adjusted_hours` místo
+# # 24 hodinového formátu (např.: 17) uložil formát anglický (např.: 5).
+# if int(hours) > 12:
+#     adjusted_hours = [str(int(hours) - 12), ':', mins, ' PM']
+# else:
+#     adjusted_hours = [hours, ':', mins, ' AM']
+#
+# # Do proměnné `daytime` vyber odpovídající string z dvojčlenného listu ['AM', 'PM']
+#
+# # Vytiskni převedený čas.
+# print('converted to eng format: ' + ''.join(adjusted_hours))
+# ================================================================
 
-# Rozděl do listu vstup od uživatele do proměnné `hours` a `mins`.
-time = time.partition(':')
-hours = time[0]
-mins = time[2]
+# Získej vstup uživatele do proměnné 'time'
+time = input('Please enter your time: ')
 
+# Rozděl do listu vstup od uživatele do proměnné 'hours` a `mins`.
+hours, mins = time.split(':')
 
-# Uprav proměnou `hours` tak, aby se do proměnné `adjusted_hours` místo
-# 24 hodinového formátu (např.: 17) uložil formát anglický (např.: 5).
-if int(hours) > 12:
-    adjusted_hours = [str(int(hours) - 12), ':', mins, ' PM']
-else:
-    adjusted_hours = [hours, ':', mins, ' AM']
+# Uprav proměnou 'hours' tak, aby se do proměnné 'adjusted_hours' místo 24 hodinového formátu (např.: 17)
+# uložil formát anglický (např.: 5).
+adjusted_hours = hours if int(hours) == 12 else str(int(hours) % 12)
 
-# Do proměnné `daytime` vyber odpovídající string z dvojčlenného listu ['AM', 'PM']
+# Do proměnné 'daytime' vyber odpovídající string z dvojčlenného listu ['AM', 'PM']
+# Dosáhneš toho pomocí výrazu, který vrátí index 0 (=False), nebo 1 (=True).
+daytime = ['AM', 'PM'][int(hours) >= 12]
 
 # Vytiskni převedený čas.
-print('converted to eng format: ' + ''.join(adjusted_hours))
+print('Converted to English format: ' + adjusted_hours + ':' + mins + ' ' + daytime)
