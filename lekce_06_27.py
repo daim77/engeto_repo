fb70300dad8a73288aea0d23831a7207c4e735b6
Martin Danek <martindanek4@gmail.com>
2020-12-11 17:26:50 +0100

    final

--- lekce_06_27.py
+++ lekce_06_27.py
@@ -6,19 +6,33 @@
 # desk - reprezentace šachovnice, tuple, list, dict
 
 desk = list()
+desk_row = []
 # Zadej rozměry šachovnice
 size = int(input('Zadej rozmer sachovnice: '))
 
-# Zadej symboly
-symbols = input('Zadej dva symboly sachovnice repre B&W: ')
+# Zadej dva symboly
+symbols = [' ', ' ', ' ']
+while len(symbols) != 2:
+    symbols = list(input('Zadej dva symboly sachovnice repre B&W: '))
 
 # Vytvoř šachovnici
 
 # Doplň smyčky for, které by měly postupně nahrát celou
 # šachovnici do proměnné 'desk'
-for a in range(size): # enumerate
-    for b in range(size):
-        desk_row[b] = symbols
-    desk[a].append(desk_row)
+for row in range(1, size + 1):
+    for item in range(1, size + 1):
+        if row % 2 != 0 and item % 2 != 0:
+            desk_row.append(symbols[0])
+        elif row % 2 != 0 and item % 2 == 0:
+            desk_row.append(symbols[1])
+        elif row % 2 == 0 and item % 2 != 0:
+            desk_row.append(symbols[1])
+        else:
+            desk_row.append(symbols[0])
+    desk.append(desk_row)
+    desk_row = []
 # Vytiskni šachovnici.
-print(symbols)
+for row in range(size):
+    for item in range(size):
+        print(desk[row][item], end='')
+    print('')
