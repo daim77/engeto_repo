2adbda32518a3918da3cd68315503ebc54eab373
Martin Danek <martindanek4@gmail.com>
2020-12-11 22:56:05 +0100

    v2.0

--- lekce_06_29.py
+++ lekce_06_29.py
@@ -27,23 +27,51 @@
 #     sorted_names.remove('')
 # print(sorted_names)
 # =========================================
-names = ['Michal', 'Pepa', 'Honza',
-        'Pavel', 'Lukas', 'Matej',
-        'Iva', 'Klara', 'Jana',
-        'Honza', 'Vasek','Milan', 'Michal']
+# names = [
+#     'Michal', 'Pepa', 'Honza',
+#     'Pavel', 'Lukas', 'Matej',
+#     'Iva', 'Klara', 'Jana',
+#     'Honza', 'Vasek', 'Milan',
+#     'Michal'
+# ]
+#
+# # Vytvoř list, do kterého vložíš jeden prvek z list `names`. Zároveň ho z listu `names` odstraň.
+# # Tento krok se ti bude hodit, když budeš chtít přidávat a seřazovat další jméno z listu `names` do listu `sorted_names`
+# sorted_names = [names.pop()]
+#
+# # Začni vnější for loop, kterým budeš procházet seznam `names` (měl by mít už o jeden prvek méně)
+# for name in names:
+#     # Zační vnitřní for loop, kterým budeš procházet seznam `sorted_names` a pomocí podmínkového výrazu,
+#     # `break` a `else` vlož jméno z `names` buď na pozici, nebo za pozici daného jméno v listu `sorted_names`
+#     for i, s_name in enumerate(sorted_names):
+#         if name < s_name:
+#             sorted_names.insert(i, name)
+#             break
+#     else:
+#         sorted_names.append(name)
+#
+# # Vytiskni seřazená jména
+# print(sorted_names)
 
-# Vytvoř list, do kterého vložíš jeden prvek z list `names`. Zároveň ho z listu `names` odstraň. Tento krok se ti bude hodit, když budeš chtít přidávat a seřazovat další jméno z listu `names` do listu `sorted_names`
-sorted_names = [names.pop(0)]
-
-# Začni vnější for loop, kterým budeš procházet seznam `names` (měl by mít už o jeden prvek méně)
+# =================================
+# # v2.0
+names = [
+    'Michal', 'Pepa', 'Honza',
+    'Pavel', 'Lukas', 'Matej',
+    'Iva', 'Klara', 'Jana',
+    'Honza', 'Vasek', 'Milan',
+    'Michal'
+]
+sorted_names = list()
+sorted_names.append(names.pop())
 for name in names:
-    # Zační vnitřní for loop, kterým budeš procházet seznam `sorted_names` a pomocí podmínkového výrazu, `break` a `else` vlož jméno z `names` buď na pozici, nebo za pozici daného jméno v listu `sorted_names`
-    for i,s_name in enumerate(sorted_names):
-        if name < s_name:
-            sorted_names.insert(i,name)
+    i = -1
+    for sorted_name in sorted_names:
+        if name < sorted_name:
+            i += 1
+            sorted_names.insert(i, name)
             break
     else:
         sorted_names.append(name)
-
-# Vytiskni seřazená jména
 print(sorted_names)
+
