9dee4fd62712a03300a40d5bdc657d35595058cd
Martin Danek <martindanek4@gmail.com>
2020-12-14 11:01:59 +0100

    final

--- lekce_06_32.py
+++ lekce_06_32.py
@@ -11,3 +11,21 @@
 # vytisknout list v rozmezí čísel start - stop, která jsou dělitelná vstupem divisor
 # Pokud je dělitel 0, program by měl vytisknout string Cannot divide by zero.
 
+list_of_quotients = list()
+start = int(input('Insert beginning of interval: '))
+end = int(input('Insert beginning of interval: '))
+
+if end < start:
+    print('Wrong interval!')
+    exit()
+
+divisor = int(input('Insert divisor: '))
+
+while divisor == 0:
+    print('Cannot divide by zero. Insert divisor once again!')
+    divisor = int(input('Insert divisor: '))
+
+for i in range(start, end + 1):
+    if i % divisor == 0:
+        list_of_quotients.append(i)
+print('Numbers in', range(start, end), 'divisible by', divisor, ':\n', list_of_quotients)
