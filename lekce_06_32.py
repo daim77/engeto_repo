b2a7d38f83ffb8f08096495e2e4f0b53ec3919bb
Martin Danek <martindanek4@gmail.com>
2020-12-14 10:43:48 +0100

    intro

---
+++ lekce_06_32.py
@@ -0,0 +1,13 @@
+# Napiš program, který bude mít 3 vstupy a všechny budou celá čísla:
+#
+# start
+# stop
+# divisor (dělitel)
+# Všechny by měly být poskytnuty uživatelem.
+#
+# Program by měl:
+#
+# vygenerovat sbírku celých čísel v rozmezí mezi start (včetně) a stop (včetně)
+# vytisknout list v rozmezí čísel start - stop, která jsou dělitelná vstupem divisor
+# Pokud je dělitel 0, program by měl vytisknout string Cannot divide by zero.
+
