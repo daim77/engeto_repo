4cf3585d7b3298c60421e1a01ce103a076e1a2a9
Martin Danek <martindanek4@gmail.com>
2020-12-14 09:56:34 +0100

    intro

---
+++ lekce_06_31.py
@@ -0,0 +1,5 @@
+Napiš skript, který spočítá kolik samohlásek a souhlásek obsahuje následující string s anglickou větou:
+# output:
+# No. consonants: 43 | No. vowels: 30
+
+sentence = 'A speech sound that is produced by comparatively open configuration of the vocal tract.'
