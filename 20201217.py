f2433aa12408b66b8eb8a6ac8c92d9ec106dfffc
Martin Danek <martindanek4@gmail.com>
2020-12-17 19:24:41 +0100

    *_

--- 20201217.py
+++ 20201217.py
@@ -1,1 +1,9 @@
-# lekce s lektorem
+# lekce s lektorem - funkce
+# opakovani for a while loop
+# .title, . lower, dict, str, float, enumerate a default hodnota, . get(var, 0)
+
+list1 = 'ahoj'
+print([i for i in list1])
+
+prvni, *_ = 'AHOJ'
+print(prvni)
