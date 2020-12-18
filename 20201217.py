e7844acb62eaf6662559f0411e38c81345a69451
Martin Danek <martindanek4@gmail.com>
2020-12-18 20:22:05 +0100

    lekce s Jakubem

--- 20201217.py
+++ 20201217.py
@@ -2,8 +2,23 @@
 # opakovani for a while loop
 # .title, . lower, dict, str, float, enumerate a default hodnota, . get(var, 0)
 
-list1 = 'ahoj'
-print([i for i in list1])
+# list1 = 'ahoj'
+# print([i for i in list1])
+#
+# prvni, *_ = 'AHOJ'
+# print(prvni)
 
-prvni, *_ = 'AHOJ'
-print(prvni)
+# def muj_sum(list_cisel:list) -> None:
+#     soucet = 0
+#     for cislo in list_cisel:
+#         soucet += cislo
+#     print(soucet)
+#
+# print(muj_sum(range(100)))
+
+# def opakuj_znak(znak, pocet_opakovani):  # parametry funkce
+#     return znak * pocet_opakovani
+#
+# print(opakuj_znak('=', 50))  # argumenty fce
+
+
