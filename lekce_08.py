d1e660d22a5b84ac2c7afc0f51fc5bce94bf49b2
Martin Danek <martindanek4@gmail.com>
2020-12-28 17:44:09 +0100

    game Sibenice final final

--- lekce_08.py
+++ lekce_08.py
@@ -274,23 +274,21 @@ def change_blind(word_to_list_blind, word_to_list, letter):
 
 
 def sibenice(words: list):
-    my_sep = '.' * 70
+    my_sep = '.' * 71
     word = random.choice(words).lower()
     word_to_list = list(word)
     word_to_list_blind = ['_'] * len(word)
     count = min(len(word) + 5, int(len(word) * 1.6), 15)
-    print(f'''
-{my_sep}
-You have total {count} attempts to guess one word. Only lowercase. Good LUCK
-{my_sep}\n
-{''.join(word_to_list_blind)}
-    ''')
+    print(my_sep)
+    print('You have total', count, 'attempts to guess one word. Only lowercase. Good LUCK')
+    print(my_sep)
+    print(' '.join(word_to_list_blind))
     while count:
         letter = ask_letter(count)
         if letter in word_to_list:
             word_to_list_blind = change_blind(word_to_list_blind, word_to_list, letter)
         print(my_sep)
-        print(''.join(word_to_list_blind))
+        print(' '.join(word_to_list_blind))
         if word_to_list_blind == list(word):
             return print(my_sep, '\nBravo. You are WINNER')
         count -= 1
@@ -301,5 +299,5 @@ sibenice([
     'Faith', 'confidence', 'concept', 'cumulative', 'context', 'belief', 'doctrines', 'Religious', 'degree', 'Stage',
     'Synthetic', 'information', 'personification', 'knowledge', 'teacher', 'Jesus', 'God', 'Sabbath', 'country',
     'weekend', 'benefits', 'administration', 'damage', 'unemployment', 'example', 'public', 'election',
-    'pneumonoultramicroscopicsilicovolcanoconiosis', 'hangman', 'aircraft', 'airbase', 'python', 'apple'
+    'pneumonoultramicroscopicsilicovolcanoconiosis', 'hangman', 'aircraft', 'airbase', 'python', 'apple', 'car'
 ])
