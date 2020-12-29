e04246efc842ae7bc173d949b06695d6c551726c
Martin Danek <martindanek4@gmail.com>
2020-12-29 17:16:51 +0100

    string format

--- lekce_09.py
+++ lekce_09.py
@@ -1,4 +1,21 @@
 # Formátování stringů a textové soubory
 
-person_data = {'name': 'Bob', 'age': 37}
-print('My name is %(name)s and I am %(age)d years old.' % person_data)
+# person_data = {'name': 'Bob', 'age': 37}
+# print('My name is %(name)s and I am %(age)d years old.' % person_data)
+# # nebo
+#
+#
+# print('My name is {name} and I am {age} years old.'.format(name='Bob', age=37))
+#
+#
+# name = 'Bob'
+# age = 37
+# print('My name is {name} and I am {age} years old.'.format(name=name, age=age))
+#
+#
+# person_data = {'name': 'Bob', 'age': 37, 'job': 'freelancer'}
+# print('My name is {name} and I am {age} years old.'.format(**person_data))
+
+
+data = ('Bob', 37.123456789, 'freelancer')
+print('|name: {0:->10}|age: {1:#>+10.5}|'.format(*data))  # index:fill|align|sign|width|.precision|
