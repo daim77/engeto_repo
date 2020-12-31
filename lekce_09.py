930de44be8c31c49074b27b22e698a11bd4ae037
Martin Danek <martindanek4@gmail.com>
2020-12-31 09:50:37 +0100

    filesystem eval from txt file

--- lekce_09.py
+++ lekce_09.py
@@ -25,9 +25,71 @@
 # precision       ::=  digit+
 # type            ::=  "b" | "c" | "d" | "e" | "E" | "f" | "F" | "g" | "G" | "n" | "o" | "s" | "x" | "X" | "%"
 
-data = ('car', 1037.123456789, 'freelancer')
-print('|word: {0:*^11}|number: {1:#>+15,.7E}|'.format(*data))
-print('|{0:*<6c}|'.format(90))
+# data = ('car', 1037.123456789, 'freelancer')
+# print('|word: {0:*^11}|number: {1:#>+15,.7E}|'.format(*data))
+# print('|{0:*<6c}|'.format(90))
+#
+# # %[(klic)][zarovnani_vypln][sirka][.presnost][typecode]
+# print('|%#5x|' % (54,))
 
-# %[(klic)][zarovnani_vypln][sirka][.presnost][typecode]
-print('|%#5x|' % (54,))
+# # Hodnoty a sirky
+# num1 = 45 / 3
+# width1 = len(str(num1)) + 2
+# num2 = 45 * 3
+# width2 = len(str(num2)) + 2
+# num3 = 45 % 3
+# width3 = len(str(num3)) + 2
+#
+# # Dynamicke formatovani sirky
+# print('|%-*d|%0*d|%-*d|%+*d|% *d|' % (width1, num1, width1, num2, width2, num3, width3, 45678, width2, 45))
+
+
+# import sys
+#
+#
+# file = open(r'/Users/martindanek/Documents/programovani/files/txt/test_text.txt')
+# print(file.tell())
+# text = file.read()
+# print(file.tell())
+# file.seek(0)
+# print(file.tell())  # possible read again. cursor position = 0
+# text_list = []
+# for char in text.split('\n'):
+#     text_list.append(char.split())
+# print(text_list)
+# print(sys.getsizeof(file))  # vraci velikost spotrebovane pameti, ne velikost souboru...
+# file.close()
+
+# # kontext automaticky zavre soubor
+# with open(r'/Users/martindanek/Documents/programovani/files/txt/test_text.txt', 'r+') as file:
+#     number_openning = file.readline()
+#     number_openning = int(number_openning.strip('\n'))
+#     number_openning += 1
+#     file.seek(0)
+#     file.write(str(number_openning))
+#     file.seek(0, 2)
+#     file.write(f'\nThis file were opened {number_openning} times')
+
+# filesystem path existence
+
+# Funkce main
+
+
+def main(my_path: str):
+    file_exists(my_path)
+    print('main')
+
+# Funkce file_exists
+
+
+def file_exists(my_path):
+    with open('/Users/martindanek/Documents/programovani/files/txt/filesystem.txt') as file:
+        filesystem = file.read()
+    print(my_path)
+    print((filesystem))
+    return
+
+
+# {'/'
+main('/bin/mkdir')
+
