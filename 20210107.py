e9e76195c609bb1822332e4292dc874eed7b9da6
Martin Danek <martindanek4@gmail.com>
2021-01-07 20:06:20 +0100

    *arg

--- 20210107.py
+++ 20210107.py
@@ -20,54 +20,61 @@
 # ==== MODULY ====
 # secti vsechna cisla ve vygenerovanem silenem stringu
 
-import random
-import string
-# import re
-
-def vytvor_sileny_string(r, z, max_cislo):
-    celkovy_text = ''
-    for radek in range(r):
-        text_pro_radek = ''
-        for znak in range(z):
-            text_pro_radek += random.choice(string.ascii_lowercase) \
-                            + str(random.randint(0, max_cislo)) \
-                            + random.choice(string.punctuation)
-        celkovy_text += text_pro_radek + '\n'
-    return celkovy_text
-
-
-def count_num(my_string: str):
-    total_sum = 0
-    while my_string:
-        number = '0'
-        while my_string[0].isdigit():
-            num = my_string[0]
-            number += num
-            my_string = my_string[1:]
-        total_sum += int(number)
-        my_string = my_string[1:]
-    return total_sum
-
-
-# def count_from_list(my_string: str):
-#     sum_list = []
-#     while my_string:
-#         count = 0
-#         char = my_string[0]
-#         if char not in ('0987654321'):
-#             my_string =
+# import random
+# import string
+# # import re
 #
-#     sum_list = my_string.split()
+# def vytvor_sileny_string(r, z, max_cislo):
+#     celkovy_text = ''
+#     for radek in range(r):
+#         text_pro_radek = ''
+#         for znak in range(z):
+#             text_pro_radek += random.choice(string.ascii_lowercase) \
+#                             + str(random.randint(0, max_cislo)) \
+#                             + random.choice(string.punctuation)
+#         celkovy_text += text_pro_radek + '\n'
+#     return celkovy_text
+#
+#
+# def count_num(my_string: str):
+#     total_sum = 0
+#     while my_string:
+#         number = '0'
+#         while my_string[0].isdigit():
+#             num = my_string[0]
+#             number += num
+#             my_string = my_string[1:]
+#         total_sum += int(number)
+#         my_string = my_string[1:]
+#     return total_sum
+#
+#
+# # def count_from_list(my_string: str):
+# #     sum_list = []
+# #     while my_string:
+# #         count = 0
+# #         char = my_string[0]
+# #         if char not in ('0987654321'):
+# #             my_string =
+# #
+# #     sum_list = my_string.split()
+#
+#
+# def main():
+#     r = int(input('Pocet radku: '))
+#     z = int(input('Pocet znaku v radku: '))
+#     max_cislo = int(input('Rozsah pro generovani cisla int: '))
+#     my_string = vytvor_sileny_string(r, z, max_cislo)
+#     print(my_string)
+#     print(count_num(my_string))
+#
+#
+# main()
 
 
-def main():
-    r = int(input('Pocet radku: '))
-    z = int(input('Pocet znaku v radku: '))
-    max_cislo = int(input('Rozsah pro generovani cisla int: '))
-    my_string = vytvor_sileny_string(r, z, max_cislo)
-    print(my_string)
-    print(count_num(my_string))
+def my_sum(*args):
+    result = sum([num for num in args])
+    return result
 
 
-main()
-
+print(my_sum(*range(4)))
