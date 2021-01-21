bff0a088fea40538765bc047352a0037fef1aae7
Martin Danek <martindanek4@gmail.com>
2021-01-21 11:14:54 +0100

    modul os

--- delete.py
+++ delete.py
@@ -1,11 +1,12 @@
-from datetime import datetime as dt
+# from datetime import datetime as dt
+#
+#
+# def day_today():
+#     ts1 = dt.now()
+#     return ts1.strftime('%Y-%b-%d %H:%M:%S')
+#
+#
+# if __name__ == '__main__':
+#     print(day_today())
+#     print(__name__)
 
-
-def day_today():
-    ts1 = dt.now()
-    return ts1.strftime('%Y-%b-%d %H:%M:%S')
-
-
-if __name__ == '__main__':
-    print(day_today())
-    print(__name__)
--- lekce 13.py
+++ lekce 13.py
@@ -1,5 +1,27 @@
-import delete
+# import delete
+#
+# if __name__ == '__main__':
+#     print(delete.day_today())
+#     print(delete.__name__)
 
-if __name__ == '__main__':
-    print(delete.day_today())
-    print(delete.__name__)
+# import os
+# from pprint import pprint as pp
+#
+#
+# pp(os.listdir())
+# print(os.getcwd())
+# pp(os.listdir('/'.join(os.getcwd().split('/')[:3])))
+# print('/'.join(os.getcwd().split('/')[:4]))
+
+import os
+
+
+try:
+    os.mkdir('test')
+except FileExistsError:
+    print()
+finally:
+    new_file = open('test/test_file.txt', 'w')
+    new_file.close()
+
+print(os.listdir('test'))
