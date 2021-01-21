2fc13bc0063b19f7ce36bd7d1a1380bd6131f050
Martin Danek <martindanek4@gmail.com>
2021-01-21 10:42:27 +0100

    moduls import

--- delete.py
+++ delete.py
@@ -1,4 +1,11 @@
-import datetime
+from datetime import datetime as dt
 
-ts1 = datetime.datetime.now()
-print(ts1.strftime('%Y-%b-%d %H:%M:%S'))
+
+def day_today():
+    ts1 = dt.now()
+    return ts1.strftime('%Y-%b-%d %H:%M:%S')
+
+
+if __name__ == '__main__':
+    print(day_today())
+    print(__name__)
---
+++ lekce 13.py
@@ -0,0 +1,5 @@
+import delete
+
+if __name__ == '__main__':
+    print(delete.day_today())
+    print(delete.__name__)
