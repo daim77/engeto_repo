80d41396c0f91047faee1820dff534c7e97aa80d
Martin Danek <martindanek4@gmail.com>
2021-01-26 20:17:49 +0100

    json modul

--- lekce 14.py
+++ lekce 14.py
@@ -25,32 +25,60 @@
 # for item in csv.DictReader(file):
 #     pp(item)
 # file.close()
+#
+#
+# import json
+# from pprint import pprint as pp
+#
+#
+# employee = {
+#     'Job': 'Programmer',
+#     'Age': 38,
+#     'Full Name': 'Galagher, Fred',
+#     'City': 'London',
+#     'Surname': 'Galagher',
+#     'Gender': 'Male',
+#     'Name': 'Fred'
+# }
+#
+# with open('/Users/martindanek/Documents/programovani/files/json/example1.json', 'w+') as file:
+#     to_json = json.dumps(employee, indent=2, sort_keys=False)
+#     # file.write(to_json)
+#     json.dump(employee, file, indent=4, sort_keys=True)
+#     print(to_json)
+#
+# # file = open('/Users/martindanek/Documents/programovani/files/json/example1.json')
+# # file_csv = file.read()
+# # from_json = json.loads(file_csv)
+#
+#     file.seek(0)
+#     # file_json = file.read()
+#     # from_json = json.loads(file_json)
+#     from_json = json.load(file)
+#     pp(from_json)
+# # file.close()
+
+# ===== Precteni prvnich sloupcu a tisk listu =====
+
+import csv
 
 
-import json
-from pprint import pprint as pp
+def read_json_column(path: str, column=0):
+    try:
+        with open(path) as file:
+            read_json = csv.reader(file)
+
+            for row in read_json:
+                if column == 0:
+                    print(' '.join(row))
+                else:
+                    print(row[column - 1])
+
+    except FileNotFoundError:
+        print('File not found')
+    except IndexError:
+        print('Wrong column')
 
 
-employee = {
-    'Job': 'Programmer',
-    'Age': 38,
-    'Full Name': 'Galagher, Fred',
-    'City': 'London',
-    'Surname': 'Galagher',
-    'Gender': 'Male',
-    'Name': 'Fred'
-}
-
-with open('/Users/martindanek/Documents/programovani/files/json/example1.json', 'w+') as file:
-    to_json = json.dumps(employee)
-    file.write(to_json)
-
-# file = open('/Users/martindanek/Documents/programovani/files/json/example1.json')
-# file_csv = file.read()
-# from_json = json.loads(file_csv)
-
-    file.seek(0)
-    file_json = file.read()
-    from_json = json.loads(file_json)
-    pp(from_json)
-# file.close()
+if __name__ == '__main__':
+    read_json_column('/Users/martindanek/Documents/programovani/files/csv/pokus.csv', 0)
