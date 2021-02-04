1d35c1ae0b5a28402a3d5eeabc360230b489d25c
Martin Danek <martindanek4@gmail.com>
2021-02-04 21:40:51 +0100

    lekce engeto

--- 20210204.py
+++ 20210204.py
@@ -3,20 +3,26 @@
 # ===== ze zadane tabulky do txt a opacne =====
 # pouzij aspon jednu vyjimku
 
-tabulka = [
-    ["Jmeno", "Prijmeni", "Vek"], ["Petr","Novotny", 45],
-    ["Hanka","Bila", 33], ["Sabina","Suchanova", 11]
-]
+# [["Jmeno", "Prijmeni", "Vek"], ["Petr","Novotny", 45], ["Hanka","Bila", 33], ["Sabina","Suchanova", 11]]
+
+# TODO: nastudovat modul logging
+
+
+import time
+# import logging
+# import sys
+import os
 
 
 def vytvor_file(data: list, cesta:str) -> None:
     with open(cesta + '/tabulka.txt', mode='w') as file:
-        for item in tabulka:
+        for item in data:
             file.write('.'.join([str(part) for part in item]) + '\n')
 
 
 def precti_file_a_vytvor_list(cesta: str) -> list:
     try:
+        # logging.basicConfig(level=logging.DEBUG, filename='myapp.log')
         with open(cesta + '/tabulka.txt', mode='r') as file:
             result = []
             while True:
@@ -29,14 +35,30 @@ def precti_file_a_vytvor_list(cesta: str) -> list:
                 except ValueError:
                     pass
                 result.append(row)
-        print(result)
+        return result
     except FileNotFoundError:
         print('soubor neexistuje')
 
 
-# def main()
-#     pass
+def main():
+    while True:
+        mode = input('Zadej mod(w/r or any as QUIT): ').lower()
+        if mode =='r':
+            list_listu = precti_file_a_vytvor_list('/Users/martindanek/Documents/programovani/files/txt')
+            print(list_listu)
+        elif mode == 'w':
+            tabulka = eval(input('Vloz svuj list listu: '))
+            vytvor_file(tabulka, '/Users/martindanek/Documents/programovani/files/txt')
+        else:
+            return
+
 
 if __name__ == '__main__':
-    vytvor_file(tabulka, '/Users/martindanek/Documents/programovani/files/txt')
-    precti_file_a_vytvor_list('/Users/martindanek/Documents/programovani/files/txt')
+    # print(sys.argv)
+    print(os.getcwd())
+    # os.chdir('/Users/martindanek/Documents/programovani/files/')
+    # os.mkdir("textove_tabulky")
+    start = time.time()
+    main()
+    end = time.time()
+    print(end - start)
