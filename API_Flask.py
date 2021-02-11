bf21f58897f839d58056104f3a2be5bfd27dfe51
Martin Danek <martindanek4@gmail.com>
2021-02-11 10:04:57 +0100

    NILL

--- API_Flask.py
+++ API_Flask.py
@@ -1,11 +1,24 @@
-from flask import Flask
+# from flask import Flask
+#
+#
+# app = Flask(__name__)
+#
+# @app.route('/', methods=['GET'])
+# def home():
+#     return 'Hello Flask'
+#
+#
+# app.run(port=3333, debug=True)
+
+from flask import Flask, escape, request
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    return 'Hello Flask'
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
 
 
 app.run(port=3333)
--- delete.py
+++ delete.py
@@ -1,16 +1,46 @@
-import tkinter as tk
-import sys
 
-window = tk.Tk()
-window.grid_columnconfigure(0, weight=1)
-window.title("Lambda")
-window.geometry("300x100")
-label = tk.Label(window, text="Lambda Calculus")
-label.grid(column=0, row=0)
-button = tk.Button(
-    window,
-    text="Reverse",
-    command=lambda: label.configure(text=label.cget("text")[::-1]),
-)
-button.grid(column=0, row=1)
-window.mainloop()
+friendships = [
+    ('A', 'B'),
+    ('B', 'C'),
+    ('A', 'C'),
+    ('C', 'D'),
+    ('B', 'F'),
+    ('D', 'E'),
+    ('F', 'E'),
+    ('F', 'J'),
+    ('E', 'H'),
+    ('E', 'G'),
+    ('G', 'H'),
+    ('H', 'I'),
+    ('I', 'J'),
+    ('I', 'L'),
+    ('L', 'M'),
+    ('M', 'N'),
+    ('N', 'O')
+]
+
+def get_friends(person):
+    my_set = set()
+    for item in friendships:
+        if person in item:
+            my_set.update(set(item).difference(person))
+    return my_set
+
+
+def common_friends(person1, person2):
+    set1 = get_friends(person1)
+    set2 = get_friends(person2)
+    return set1 & set2
+
+
+def is_friendship(person1, person2):
+    for item in friendships:
+        if item == (person1, person2) or item == (person2, person1):
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    print(common_friends('G', 'H'))
+    print(is_friendship('A', 'D'))
